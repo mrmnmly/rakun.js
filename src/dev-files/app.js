d4a92ca5f5168fa7375ab2b6c162d9ae3d30ee98
mrmnmly <lukaszkups@gmail.com>
2016-12-09 00:03:28 +0100

    temp commit

--- src/dev-files/app.js
+++ src/dev-files/app.js
@@ -1,30 +1,6 @@
 (function(){
-	var app = new Rcn();
-	var appRouter = app.newRouter();
+	var _app = new Rcn();
+	var _appRouter = _app.newRouter();
 
-	var homeRoute = appRouter.newRoute({
-		name: 'home',
-		url: '/',
-		renderData: function(){
-			return {name: "lukasz", technologies: ['javascript', 'node.js', 'phonegap', 'express.js']};
-		},
-		wrapper: 'id-home-wrapper',
-		beforeRender: function(template){
-			// renderData.technologies.push('git');
-			// console.log(renderData.name);
-			template.querySelector('strong').innerText += ' olaboga!';
-		},
-		template: '<strong>BLA!</strong>'
-	});
-
-	if(window.location.hash == '#/'){
-		appRouter.activateRoute(homeRoute);
-	}else{
-		// TODO: write function that gets current window.location.hash and search for proper route
-		appRouter.detectRoute();
-	}
-
-
-
-	// window.location.href = window.location.pathname;
+	var home = new Home(_app, _appRouter);
 }());
--- src/rakun.js
+++ src/rakun.js
@@ -1,140 +1,238 @@
-// Rcn a.k.a. Rakun a.k.a. Raccoon
 function Rcn(){
-	var _self = this;
+  var _self = this;
+  // array of all states
+  _self.stateBox = [];
+  // array of all routes
+  _self.routeBox = [];
 
-	// add '#' to app url on init
-	if(window.location.href.indexOf('#/') < 0){
-		window.location.href += '#/';
-		// url += '#/';
-		// window.location.href = url;
-	}
+  // add '#' to app url on init
+  _self.addHashToUrl = (function(){
+    if(window.location.href.indexOf('#/') < 0){
+      var url = window.location.href.replace(/#\//g, '');
+      url += '#/';
+      window.location.href = url;
+    }
+  }());
+
+  return _self;
 }
 
+// router manages all the routes
 Rcn.prototype.newRouter = function(){
-	console.log('new router!')
-	var _self = this;
-	var _routes = [];
+  var _self = this;
+  var routes = _self.routeBox;
 
+  // binding hash change event
+  _self.bindHashChangeEvent = (function(){
+    window.addEventListener('hashchange', function(){
+      if(!window.location.href || !window.location.href.indexOf('#') < 0){
+        throw new Error('Something went wrong (missing \'#\')');
+      }
+      _self.findRoute(window.location.href.split('#')[1], true);
+    });
+  }());
 
-	// TODO:
-	// pass in newRouter option params error route templates (e.g. 404, 500 etc.)
+  // create new route and add it to _self.routeBox array (array of all routes)
+  _self.newRoute = function(options){
+    var self = this;
 
+    // newRoute options validation
+    if(!options){
+      throw new Error('No route params provided!');
+    }
+    if(!options.name || !options.name.length){
+      throw new Error('No route name specified!');
+    }
+    if(options.name.indexOf('/') > -1){
+      throw new Error('Route name shouldn\'t contain slash (/) character!');
+    }
+    if(!options.url || !options.url.length){
+      throw new Error('No route url specified!');
+    }
+    if(!options.wrapper || !options.wrapper.length){
+      throw new Error('No route wrapper DOM id specified!');
+    }
+    if(!document.getElementById(options.wrapper)){
+      throw new Error('Route DOM wrapper not found in document!');
+    }
+    if(!options.template && !options.templateUrl){
+      console.log(options)
+      throw new Error('No route template specified!');
+    }
+    if(options.template && options.templateUrl){
+      throw new Error('Template and templateUrl specified at the same time!');
+    }
 
-	window.onload = function(){
-		if(_routes.length){
-			self.detectRoute();
-			// TODO activate route
-		}
-	};
+    // add newRoute object to _self.routeBox array (array of all routes)
+    _self.routeBox.push(options);
 
+    // reurn newRoute object
+    return self;
+  }
 
-	// binding event listener on hash location change automatically when create new router
-	window.addEventListener('hashchange', function(){
-		console.log('location changed!');
-		if(!window.location.href || !window.location.href.indexOf('#') < 0){
-			throw new Error('Something went wrong with url (# mark is missing).');
-		}
+  // choose which mechanism to use depends on provided routeToFind param (route name or route url)
+  // route name will not contain slash character (/)
+  _self.findRoute = function(routeToFind, redirect){
+    if(routeToFind || routeToFind.length || typeof routeToFind === 'string'){
+      if(routeToFind.indexOf('/') > -1){
+        return _self.findRouteByUrl(routeToFind, redirect);
+      }else{
+        return _self.findRouteByName(routeToFind, redirect);
+      }
+    }else{
+      throw new Error('No route to find specified or is not a string!');
+    }
+  }
 
-		_self.checkRoute();
-	});
+  // find current route in _self.routeBox array (array of all routes) by given url
+  _self.findRouteByUrl = function(href, redirect){
+    var self = this;
+    // validation
+    if(!href || !href.length || typeof href != 'string'){
+      throw new Error('No route href specified or is not a string!');
+    }
+    var currentRoute = _self.routeBox.filter(function(obj){
+      return obj.url === href;
+    });
+    // validation
+    if(!currentRoute.length){
+      console.warn('Route not found!', href);
+      return;
+    }
+    // activate found route if redirect is true
+    if(redirect === true){
+      _self.activateRoute(currentRoute[0]);
+    }
+    // return found route
+    return currentRoute[0];
+  }
 
-	// function that parses current location href and search for specific route
-	_self.checkRoute = function(){
-		var self = this;
-		var currentUrl = window.location.href;
-		// get location url value after '#'
-		var currentRoute = currentUrl.split('#')[1];
+  // find current route in _self.routeBox array (array of all routes) by given route name
+  _self.findRouteByName = function(name, redirect){
+    var self = this;
+    //validation
+    if(!name || !name.length || typeof name != 'string'){
+      throw new Error('No route name specified or is not a string!');
+    }
+    var currentRoute = _self.routeBox.filter(function(obj){
+      return obj.name === name;
+    });
 
-		// probably temporary solution!
-		// TODO: fix it
-		var route = _routes.filter(function(obj){
-			return obj.url == currentRoute;
-		});
+    // validation
+    if(!currentRoute.length){
+      console.warn('Route not found!');
+      return;
+    }
+    // activate found route if redirect is true
+    if(redirect === true){
+      _self.activateRoute(currentRoute[0]);
+    }
+    // return found route
+    return currentRoute[0];
+  }
 
-		// validation
-		if(!route && !route.length){
-			// TODO: think about raising 404 error page here (if defined) instead of console error
-			throw new Error('Route not found!');
-		}
+  // activate given route in app
+  // route param can be a string [existing route name] or route object
+  _self.activateRoute = function(route, params){
+    // if given route was a string, then search for it in _self.routeBox (array of all routes)
+    if(typeof route === 'string'){
+      route = _self.findRoute(route);
+    }
+    var wrapper = document.getElementById(route.wrapper);
+    var templateFragment = document.createDocumentFragment();
+    // create container that will wrap whole template (will be removed later, after render TODO)
+    var container = document.createElement('div');
+    // wrapper validation
+    if(!wrapper){
+      throw new Error('Route wrapper does not exist in document!');
+    }
+    // template validation
+    if(route.template){
+      container.innerHTML = route.template;
+      templateFragment.appendChild(container);
+      // TODO - add code to remove holder from document fragment immediately after inserting it in above line
+    }else if(route.templateUrl){
+      // if template has not been defined, then templateUrl should be
+      // TODO: add template load and other stuff here;
+    }else{
+      throw new Error('Template not defined!');
+    }
 
-		_self.activateRoute(route[0]);
-	};
+    // handle beforeRender [optional param] method (passed templateFragmet is for accessing template from that function)  
+    if(route.beforeRender){
+      route.beforeRender(templateFragment);
+    }
 
-	// function that creates new route object and adds it to _routes array
-	// options params example:
-	_self.newRoute = function(options){
-		var self = this;
+    // for the first time, route will not be mounted, so we need to insert it into app template
+    // it will also run every time, if we set route.resetTemplate to true
+    if(!route.mounted || route.resetTemplate === true){
+      route.mounted === true;
+      wrapper.innerHTML = '';
+      wrapper.appendChild(templateFragment);
+      if(route.afterRender && typeof route.afterRender === 'function'){
+        setTimeout(function(){
+          route.afterRender();
+        }, 0);
+      }
+    }
+  }
 
-		// validation start
-		if(!options){
-			throw new Error('No newRoute() function params provided.');
-		}
-		if(!options.name || !options.name.length){
-			throw new Error('No route name specified.');
-		}
-		if(!options.url || !options.url.length){
-			throw new Error('No route url path specified.');
-		}
-		if(!options.wrapper || !options.wrapper.length){
-			throw new Error('No route wrapper dom id specified.');
-		}
-		if(!document.getElementById(options.wrapper)){
-			throw new Error('Route DOM wrapper not found in document.');
-		}
-		if(!options.template && !options.templateUrl){
-			throw new Error('You should specify template or templateUrl.');
-		}
-		if(options.template && options.templateUrl){
-			throw new Error('You cannot specify template and templateUrl at the same time.');
-		}
-		// TODO: add validation if options.beforeRender is defined, then check if it is a function
+  // redirect app using Rcn built-in mechanism (but we can also do it in traditional way)
+  _self.redirect = function(routeName, routeParams){
+    var route = _self.findRoute(routeName);
 
+    if(route && route.url){
+      console.log('redirect: ', routeName)
+      window.location.hash = route.url;
+      // _self.activateRoute(routeName, routeParams);
+    }else{
+      throw new Error('Redirection failed - route not found');
+    }
+  }
 
-		// validation end
-
-
-		// add route to _routes array
-		_routes.push(options);
-		console.log(_routes);
-
-		return options;
-	};
-
-	// activate given route in app
-	_self.activateRoute = function(route){
-		// TODO: add some function that hides all other routes (based on _routes array wrappers or something)
-
-		// TODO: add promise-based, async getting route.renderData (let's fetch it in the meantime of doing other stuff below, before handling route.beforeRender)
-
-		var wrapper = document.getElementById(route.wrapper);
-		var templateFragment = document.createDocumentFragment();
-		// create a holder node that will be a holder for our string-based HTML template code
-		var holder = document.createElement("div");
-
-		// if template was defined in route
-		if(route.template){
-			// TODO: add handling Handlebars or something in route.template variable before inserting into templateFragment
-
-			holder.innerHTML = route.template;
-			templateFragment.appendChild(holder);
-			// TODO - add code to remove holder from document fragment immediately after inserting it in above line
-		}else{
-			// if template has not been defined, then templateUrl was
-			// TODO: add template load and other stuff here;
-		}
-		// handle beforeRender method on created documentFragment
-		if(route.beforeRender){
-			// TODO: add when(route.renderData).then(..) before invoking route.beforeRender (because in future we will want to use that data in beforeRender function)
-			route.beforeRender(templateFragment);
-			setTimeout(function(){
-				wrapper.innerHTML = '';
-				console.log(templateFragment);
-				wrapper.appendChild(templateFragment);
-			}, 0);
-		}
-	}
-
-
-	// return router object
-	return _self;
+  // return new router object
+  return _self;
 }
+
+Rcn.prototype.newComponent = function(options){
+
+}
+
+// find state in _self.stateBox by given state name
+Rcn.prototype.findState = function(stateName){
+  var _self = this;
+
+  if(!stateName){
+    throw new Error('No state name provided!');
+  }
+
+  var state = _self.stateBox.filter(function(obj){
+    return obj.name = stateName;
+  });
+
+  if(state && state.length){
+    return state[0];
+  }else{
+    return;
+  }
+}
+
+Rcn.prototype.newState = function(options){
+  var _self = this;
+  var states = _self.stateBox;
+
+  // newState options validation
+  if(!options){
+    throw new Error('No state params provided!');
+  }
+  if(!options.name || !options.name.length){
+    throw new Error('No state name provided!');
+  }
+  
+
+  // add state to _self.stateBox array (array of all states)
+  states.push(options);
+  // return new state object
+  return _self;
+}
+
